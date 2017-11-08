4d2df6d695fd4a2ad59a29a33a423ac7b85b8139
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-11-08 13:52:13 -0700

    Worked on Algorithms view controller
    You moved way to fast for me

--- CSPFrameworkApp/Controller/AlgorithmViewController.swift
+++ CSPFrameworkApp/Controller/AlgorithmViewController.swift
@@ -8,28 +8,41 @@
 
 import UIKit
 
-class AlgorithmViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+public class AlgorithmViewController: UIViewController {
+    //MARK Data members
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var algorithmText: UILabel!
+    
+    private func setupAlgorithm() -> Void
+    {
+        var algorithmSteps: [String] = []
+        //MARK: Algorithm definition
+        let algorithm :String = "These are the insctions to create a project in Java using Eclipse and Getub \n"
+        let stepOne :String = "Frist,Open elcipse"
+        let stepTwo :String = "Second, Name project"
+        let stepThree: String = "Go into source and make 3 packages, view, model, and controller"
+        let stepFour: String = "In controller make name.controller and name.runner"
+        let stepFive: String = "Open get hub"
+        let stepSix: String = "Make the respository"
+        let stepSeven: String = "in runner make the runner statement"
+        let stepEight: String = "make sure to work on the view folder"
+        let stepNine: String = "Commit"
+        
+        // TODO: Finish adding all steps to the algorithm
+        algorithmSteps = [stepOne, stepTwo, stepThree, stepFour, stepFive, stepSix, stepSeven, stepEight, stepNine]
+        
+        let attributesDictionairy = [NSAttributedStringKey.font : algorithmText.font]
+        let fullAttributedString = NSMutableAttributedString(string: algorithm, attributes: attributesDictionairy)
+        
+        for step in algorithmSteps
+        {
+            let bullet: String = ""
+            let formattedStep: String = "\n\(bullet) \(step)"
+            let attributedStringStep : NSMutableAttributedString = NSMutableAttributedString(string: formattedStep)
+            let pargraphStyle = createPargraphStyle()
+            
+            attributedStringStep.addAttributes
+            
+        }
     }
-    */
-
 }
