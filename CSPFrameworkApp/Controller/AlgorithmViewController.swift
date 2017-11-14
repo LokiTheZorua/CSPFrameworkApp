3955026f3340fedadafe9a2a759940320b1c9501
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-11-14 13:48:52 -0700

    finished the world problems part.

--- CSPFrameworkApp/Controller/AlgorithmViewController.swift
+++ CSPFrameworkApp/Controller/AlgorithmViewController.swift
@@ -31,18 +31,39 @@ public class AlgorithmViewController: UIViewController {
         // TODO: Finish adding all steps to the algorithm
         algorithmSteps = [stepOne, stepTwo, stepThree, stepFour, stepFive, stepSix, stepSeven, stepEight, stepNine]
         
-        let attributesDictionairy = [NSAttributedStringKey.font : algorithmText.font]
-        let fullAttributedString = NSMutableAttributedString(string: algorithm, attributes: attributesDictionairy)
+        let attributesDictionary = [NSAttributedStringKey.font : algorithmText.font]
+        let fullAttributedString = NSMutableAttributedString(string: algorithm, attributes: attributesDictionary)
         
         for step in algorithmSteps
         {
             let bullet: String = ""
             let formattedStep: String = "\n\(bullet) \(step)"
             let attributedStringStep : NSMutableAttributedString = NSMutableAttributedString(string: formattedStep)
-            let pargraphStyle = createPargraphStyle()
+            let pargraphStyle = createParagraphStyle()
             
-            attributedStringStep.addAttributes
+            attributedStringStep.addAttributes([NSAttributedStringKey.paragraphStyle : paragraphStyle], range: NSMakeRange(0, attributedStringStep.length))
             
+            fullAttributedString.append(attributedStringStep)
         }
+        algorithmText.attributedText = fullAttributedString
+        0
+    }
+    private func createParagraphStyle() -> NSParagraphStyle
+    {
+        
+        let paragraphStyle: NSMutableParagraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .left
+        paragraphStyle.defaultTabInterval = 15
+        paragraphStyle.firstLineHeadIndent = 20
+        paragraphStyle.headIndent = 35
+        
+        return paragraphStyle
+    }
+    
+    override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        setupAlgorithm()
+        
     }
 }
--- CSPFrameworkApp/Controller/GlobalImpact.swift
+++ CSPFrameworkApp/Controller/GlobalImpact.swift
@@ -20,24 +20,23 @@ class GlobalImpact: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+        private func addImage() -> Void
+    {
+        planImage.image = UIImage(named : "projectImage")
+    }
+        private func addText() Void -> Void
+    {
+        self.collctedData.text = "This App will gather data about the size of the person's carbon foodprint and will tell them how well they did"
+        self.whyNeeded.text = "This is needed because of how much it effects the envoirment, it also is the cause of Utah's yearly inversion"
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
