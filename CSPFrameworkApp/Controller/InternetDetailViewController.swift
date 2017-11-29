76e674464ce48d9cf031124cc160de06c5feb10a
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-11-29 12:01:17 -0700

    added a new class for WebKit

---
+++ CSPFrameworkApp/Controller/InternetDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  InternetDetailViewController.swift
+//  CSPFrameworkApp
+//
+//  Created by Jones, Alec on 11/29/17.
+//  Copyright © 2017 Jones, Alec. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class InternetDetailViewController: UIViewController
+{
+    //Mark: GUI Controlls
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() -> Void
+    {
+        
+    }
+
+}
