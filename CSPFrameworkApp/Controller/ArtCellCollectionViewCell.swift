4ac4c9f66c6d8be135d6836c32cecfcf6d9e30be
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-12-07 12:54:21 -0700

    added the creattivity view Controller and mande the ArtCell Class

---
+++ CSPFrameworkApp/Controller/ArtCellCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtCellCollectionViewCell.swift
+//  CSPFrameworkApp
+//
+//  Created by Jones, Alec on 12/7/17.
+//  Copyright © 2017 Jones, Alec. All rights reserved.
+//
+
+import UIKit
+
+class ArtCellCollectionViewCell: UICollectionViewCell
+{
+
+    @IBoutlet weak var inmageView: UIImageView
+    
+    @IBOutlet weak var imageName: UILable
+}
